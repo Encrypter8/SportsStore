5eeec5f53e09ea71491108fb6d08d1411f9d30a8
Harris Miller <harrismiller08@gmail.com>
2014-02-23 19:46:27 -0700

    Unversioning Nugets

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+	public class NinjectControllerFactory : DefaultControllerFactory
+	{
+		private IKernel NinjectKernel;
+
+		public NinjectControllerFactory()
+		{
+			NinjectKernel = new StandardKernel();
+			AddBindings();
+		}
+
+		protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+		{
+			return controllerType == null ? null : (IController)NinjectKernel.Get(controllerType);
+		}
+
+		private void AddBindings()
+		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new List<Product>
+				{
+					new Product { Name = "Football", Price = 25 },
+					new Product { Name = "Surf Board", Price = 179 },
+					new Product { Name = "Running Shoes", Price = 95 }
+				}.AsQueryable()
+			);
+
+			NinjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+		}
+
+
+	}
+}
