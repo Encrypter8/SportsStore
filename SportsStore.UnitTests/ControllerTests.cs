c9aba6eabfa66c0f5ea2829650d4afacf4240262
Harris Miller <harrismiller08@gmail.com>
2014-02-24 01:42:19 -0700

    Added Cart Model Binding

--- SportsStore.UnitTests/ControllerTests.cs
+++ SportsStore.UnitTests/ControllerTests.cs
@@ -6,6 +6,7 @@ using SportsStore.WebUI.Controllers;
 using SportsStore.WebUI.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 
 namespace SportsStore.UnitTests
 {
@@ -25,6 +26,8 @@ namespace SportsStore.UnitTests
 
 			return mock;
 		}
+
+		#region ProductController
 
 		[TestMethod]
 		public void Can_Paginate()
@@ -140,5 +143,63 @@ namespace SportsStore.UnitTests
 			Assert.AreEqual(res3, 1);
 			Assert.AreEqual(resAll, 5);
 		}
+
+		#endregion
+
+		#region CartController
+
+		[TestMethod]
+		public void Can_Add_To_Cart()
+		{
+			// Arrange
+			Mock<IProductRepository> mock = GetMockIProductRepository();
+
+			Cart cart = new Cart();
+
+			CartController target = new CartController(mock.Object);
+
+			// Act
+			target.AddToCart(cart, 1, null);
+
+			// Assert
+			Assert.AreEqual(cart.Lines.Count(), 1, "Number of Cart Lines Failed");
+			Assert.AreEqual(cart.Lines.First().Product.ProductID, 1, "First Cart Line ProductID Failed");
+		}
+
+		[TestMethod]
+		public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+		{
+			// Arrange
+			Mock<IProductRepository> mock = GetMockIProductRepository();
+
+			Cart cart = new Cart();
+
+			CartController target = new CartController(mock.Object);
+
+			// Act
+			RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+			// Assert
+			Assert.AreEqual(result.RouteValues["action"], "Index");
+			Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+		}
+
+		[TestMethod]
+		public void Can_View_Cart_Contents()
+		{
+			// Arrange
+			Cart cart = new Cart();
+
+			CartController target = new CartController(null);
+
+			// Act
+			CartIndexViewModel result = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+			// Assert
+			Assert.AreSame(result.Cart, cart);
+			Assert.AreEqual(result.ReturnUrl, "myUrl");
+		}
+
+		#endregion
 	}
 }
--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -18,52 +18,44 @@ namespace SportsStore.WebUI.Controllers
 			Repo = repo;
 		}
 
-		public ViewResult Index(string returnUrl)
+		public ViewResult Index(Cart cart, string returnUrl)
 		{
 			return View(new CartIndexViewModel
 			{
-				Cart = GetCart(),
+				Cart = cart,
 				ReturnUrl = returnUrl
 			});
 		}
 
-		public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+		public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
 		{
 			Product product = Repo.Products
 				.FirstOrDefault(p => p.ProductID == productId);
 
 			if (product != null)
 			{
-				GetCart().AddItem(product, 1);
+				cart.AddItem(product, 1);
 			}
 
 			return RedirectToAction("Index", new { returnUrl });
 		}
 
-		public RedirectToRouteResult remvoeFromCart(int productId, string returnUrl)
+		public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
 		{
 			Product product = Repo.Products
 				.FirstOrDefault(p => p.ProductID == productId);
 
 			if (product != null)
 			{
-				GetCart().RemoveLine(product);
+				cart.RemoveLine(product);
 			}
 
 			return RedirectToAction("Index", new { returnUrl });
 		}
 
-		private Cart GetCart()
+		public PartialViewResult Summary(Cart cart)
 		{
-			Cart cart = (Cart)Session["Cart"];
-
-			if (cart == null)
-			{
-				cart = new Cart();
-				Session["Cart"] = cart;
-			}
-
-			return cart;
+			return PartialView(cart);
 		}
 	}
 }
